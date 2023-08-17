5e5e86cdb8984295dbe46b50b37440942c68fe55
punnavamshi <141133092+punnavamshi@users.noreply.github.com>
2023-08-17 11:40:09 +0530

    Update VM_tb.v

--- VM_tb.v
+++ VM_tb.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 23.07.2023 19:45:43
-// Design Name: 
-// Module Name: VendingMachine_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 module VendingMachine_tb();
 
 // Inputs
